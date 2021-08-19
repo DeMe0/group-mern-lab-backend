1edb6a34de014a45970ded025b6440c31f66971a
Anthony DeMeo <anthonydemeo@Anthonys-MBP.JOOWIN>
2021-08-19 10:54:19 -0400

    fixed server connection

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 ///////////////////////////
 // Environmental Variables
 ///////////////////////////
-require("dotenv").config()
-const {PORT=6280} = process.env
+require("dotenv").config();
+const { PORT = 3000 } = process.env;
 
 //MONGO CONNECTION
-const mongoose = require('./db/connection')
-const morgan = require('morgan')
-const cors = require('cors')
-const { response } = require("express")
+const mongoose = require("./db/connection");
+
+//CORS
+const cors = require("cors");
 
 //Bringing in Express
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("hello world"))
+//OTHER IMPORTS
+const morgan = require("morgan");
+const songsRouter = require("./controllers/song");
 
-app.listen(PORT, () => console.log(`port is running on ${PORT}`))
+////////////
+//MIDDLEWARE
+////////////
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny")); //logging
+
+///////////////
+//Routes and Routers
+//////////////
+
+//Route for testing server is working
+app.get("/", (req, res) => {
+  res.json({ hello: "Hello World!" });
+});
+
+// Dog Routes send to dog router
+app.use("/songs", songsRouter);
+
+//LISTENER
+app.listen(PORT, () => {
+  console.log(`Your are listening on port ${PORT}`);
+});
